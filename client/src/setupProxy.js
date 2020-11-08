c28a1ad643989dfedfd43fc81282bf7a34b7f334
ellatom <68844149+ellatom@users.noreply.github.com>
2020-11-08 13:58:10 +0200

    Add files via upload

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         ["/api"],
         createProxyMiddleware({
-            target: "http://localhost:3030",
+            target: "http://localhost:3030" || `http://localhost:${process.env.PORT}` || `http://0.0.0.0:${process.env.PORT}` ,
             changeOrigin: true 
         })
     );
